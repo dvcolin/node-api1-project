720e2002953386b0b1acd9a72d4023b0e3855076
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-02 10:34:57 -0700

    added delete request to /users/:id

--- index.js
+++ index.js
@@ -57,6 +57,26 @@ server.get('/users/:id', (req, res) => {
 })
 
 
+// DELETE REQUEST WITH USER ID
+
+server.delete('/users/:id', (req, res) => {
+    const userId = req.params.id;
+
+    Users.remove(userId)
+    .then(user => {
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The user could not be removed" });
+    })
+})
+
+
+
 
 
 
