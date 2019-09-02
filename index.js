aadc98cf1e803294cc35c51546f60352b3540eee
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-02 10:27:56 -0700

    added get request to /users/:id

--- index.js
+++ index.js
@@ -38,6 +38,25 @@ server.get('/users', (req, res) => {
 })
 
 
+// GET REQUEST WITH USER ID
+
+server.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    
+    Users.findById(userId)
+    .then(user => {
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The user information could not be retrieved." });
+    })
+})
+
+
 
 
 
